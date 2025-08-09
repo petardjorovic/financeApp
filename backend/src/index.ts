b3268180190016f131e817714fc0ffca5b7f67c7
petardjorovic <perodjorovic@gmail.com>
2025-08-09 20:07:18 +0200

    Express route & database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,34 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import connectToDatebase from "./config/db.js";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env.js";
+import cookieParser from "cookie-parser";
+import errorHandler from "./middleware/errorHandler.js";
+import catchErrors from "./utils/catchErrors.js";
+import { OK } from "./constants/http.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: APP_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.get("/", (req, res, next) => {
+  res.status(OK).json({
     message: "healty",
   });
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV} enviroment.`);
+  await connectToDatebase();
 });
---
+++ backend/src/utils/catchErrors.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+type AsyncController = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<any>;
+
+const catchErrors =
+  (controller: AsyncController): AsyncController =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export default catchErrors;
